3de24adf0061e559d0a493a9c8e38a20d1788a0c
tolgahanturker <tolgahanturker@gmail.com>
2025-04-01 22:37:58 +0300

    Argumant parsing and reading input json file

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
-from model.level import Level
-from model.phenomenon import Phenomenon
-from model.pos import POS
-from model.unit import Unit
+import argparse
+import json
 
-Level.mapLevel("HN")
-Phenomenon.mapPhenomenon("HN")
-POS.mapPOS("HN")
-Unit.mapUnit("HN")
+def main():
+    parser = argparse.ArgumentParser(description = "Process a file path.")
+    parser.add_argument("path", help = "Path to the Label Studio's json output/export file ")
+    
+    args = parser.parse_args()
+    print(f"Path received: {args.path}")
+
+    try:
+        with open(args.path, 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        print(f"Error: The file '{args.path}' was not found.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+    
+    print("Number of tasks: ", len(data))
+    
+if __name__ == "__main__":
+    main()
