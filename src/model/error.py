19a2acfc252fa4c02f1ea92ac48b2e9bb8fe25c8
tolgahanturker <tolgahanturker@gmail.com>
2025-04-14 20:45:17 +0300

    comment modification

--- src/model/error.py
+++ src/model/error.py
@@ -6,14 +6,14 @@ class Error:
         self.idData = idData # data[0]["data"]["ID"] -> id that is coming from the data itself (per text)
         self.rawText = rawText # data[0]["data"]["DATA"] -> raw text of task
         self.sentOrig = sentOrig # get_sentence_by_index(data[0]["data"]["DATA"], data[0]["annotations"][0]["result"][0]["value"]["start"]) -> sentence that the error is observed in
-        self.sentCorr = sentCorr # corrected sentence ???
+        self.sentCorr = sentCorr # corrected sentence
         self.idxStartErr = idxStartErr # data[0]["annotations"][0]["result"][0]["value"]["start"] -> index that the error starts
         self.idxEndErr = idxEndErr # data[0]["annotations"][0]["result"][0]["value"]["end"] -> index that the error ends
         self.errType = errType # data[0]["annotations"][0]["result"][0]["value"]["labels"][0] -> type of the error
         self.incorrText = incorrText # data[0]["annotations"][0]["result"][0]["value"]["text"] -> incorrect text
         self.corrText = corrText # data[0]["annotations"][0]["result"][1]["value"]["text"][0] -> corrected text
-        self.idxStartSent = idxStartSent # start idx of incorrect sentence that involves the error ???
-        self.idxEndSent = idxEndSent # end idx of incorrect sentence that involves the error ???
+        self.idxStartSent = idxStartSent # start idx of incorrect sentence that involves the error
+        self.idxEndSent = idxEndSent # end idx of incorrect sentence that involves the error
         self.errTax = errTax # taxonomic representation of the error
 
     def print(self):
