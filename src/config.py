f54fcfe562528411eec8ed52c257eba250064f8c
tolgahanturker <tolgahanturker@gmail.com>
2025-10-16 21:49:32 +0300

    changing UDPipe model from boun to imst

--- src/config.py
+++ src/config.py
@@ -4,5 +4,5 @@ DATA = [] # will hold the input data (Label Studio's json output/export file)
 METADATA = [] # used to keep metadata information
 ABBREVIATIONS_TDK = [] # used to keep abbreviations defined by TDK
 UDPIPE2_SERVICE = "https://lindat.mff.cuni.cz/services/udpipe/api"  # base URL for UDPipe service
-UDPIPE2_MODEL = "turkish-boun-ud-2.15-241121" # Turkish model for UDPipe
+UDPIPE2_MODEL = "turkish-imst-ud-2.15-241121" # Turkish model for UDPipe (old: turkish-boun-ud-2.15-241121)
 MAP_LOOKUP = [] # used to find new indices of errors in corrected text
